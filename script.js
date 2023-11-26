13c77d99407d97ec5f8a78227e98d2ff4f32f349
MD Nazirul Islam <139609914+webnpage@users.noreply.github.com>
2023-11-26 23:33:10 +0600

    hello

---
+++ script.js
@@ -0,0 +1,73 @@
+const form = document.querySelector('form');
+const todoUl = document.querySelector('#items')
+const comUl = document.querySelector('.comlist-ul')
+const inputElement = document.querySelector('input[type=text]')
+
+//create and add task here
+
+function addTask(event) {
+    event.preventDefault();
+
+    let listItem = document.createElement('li');
+    let checkbox = document.createElement('input');
+    let label = document.createElement('label');
+    
+    checkbox.type = "checkbox";
+    label.innerText = inputElement.value;
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label)
+
+
+    todoUl.appendChild(listItem)
+    inputElement.value = "";
+
+    saveData()
+} 
+
+
+form.addEventListener('submit',
+addTask)
+
+todoUl.addEventListener('click', function(event) {
+    if(event.target.tagName === "INPUT") {
+        let li = event.target.parentNode;
+        let deletebtn = document.createElement('button');
+        deletebtn.innerText = "Delete";
+        deletebtn.className = 'delete';
+    
+        let checkbox = li.querySelector('input[type="checkbox"]')
+        li.removeChild(checkbox)
+
+        li.appendChild(deletebtn)
+
+        comUl.appendChild(li)
+    }
+
+    saveData()
+} )
+
+comUl.addEventListener('click', function(event) {
+    if(event.target.tagName === "BUTTON"){
+        let li = event.target.parentNode;
+
+        comUl.removeChild(li)
+    }
+
+    saveData()
+})
+
+
+function saveData() {
+    localStorage.setItem('data1', todoUl.innerHTML)
+    localStorage.setItem('data2', comUl.innerHTML)
+    
+}
+
+
+function showData() {
+    todoUl.innerHTML = localStorage.getItem('data1')
+    comUl.innerHTML = localStorage.getItem('data2')
+}
+
+showData()
